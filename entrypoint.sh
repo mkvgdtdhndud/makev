9db31fa42b0a70f642ac71e386a94c7383fefdc9
mkvgdtdhndud <erereremkv@atomicmail.io>
2026-08-29 20:10:25 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ TOR_USER="tor"
 
 # لیست کشورها و پورت‌های خروجی Tor
 declare -A NODES=(
-     ["FR"]="9083" ["IT"]="9089" ["NO"]="9098"
+     ["AT"]="9092" ["IT"]="9089" ["NO"]="9098"
 )
 
 for code in "${!NODES[@]}"; do
